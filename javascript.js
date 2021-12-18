84ea2b03837f4d9a818e46ae9e7ec7b5b33857c3
marikadicarlo <mdicarlo19@yahoo.com>
2021-12-18 11:23:01 -0500

    more js edits

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ var quizContainer = document.getElementById("quiz");
 
 var questionList = [
     {
-    question: "Inside which HTML elemtn do we put the Javascript?",
+    question: "Inside which HTML element do we put the Javascript?",
     choices: ["<script>", "<scripting>", "<javascript>", "<js>"],
     answer: "<script>"
 },
@@ -23,6 +23,11 @@ var questionList = [
     answer: "FALSE"
 },
     {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["JavaScript", "Terminal/Bash", "For Loops", "Console Log"],
+    answer: "Console Log"
+},
+    {
     question: "What does event.preventDefault() do?",
     choices: ["It stops the browser from reloading the page upon a form submission.", "It stops the browser from allowing the form submission event to occur."],
     answer: "It stops the browser from reloading the page upon a form submission."
@@ -31,75 +36,81 @@ var questionList = [
     question: "How can you add a comment in a JavaScript?",
     choices: ["<!--This is a comment-->", "'This is a comment'", "//This is a comment"],
     answer: "//This is a comment"
-}
+},
+    {
+    question: "The condition of an 'if' / 'else' statement is enclosed within:",
+    choices: ["Quotes", "Curly Braces", "Parenthesis", "Square Brackets"],
+    answer: "Curly Braces",
+},
 ];
 
 button.addEventListener("click", function(event){
     console.log('click');
     event.stopPropagation();
     document.getElementById("startGame").style.display = "none";
-    clear = setInterval(updateCountdown, 1000),
+    button = setInterval(updateCountdown, 1000),
    showCurrentQuestion();
 })
 
-// function updateCountdown(event){
-//     currentTime--;
-//     countdown.innerHTML = ("Time Remaining" + currentTime);
+function updateCountdown(event){
+    currentTime--;
+    countdownEl.innerHTML = (currentTime);
 
-//     if (currentTime == 0){
-//         alert("Time's Up!");
-//         quizContainer.innerHTML = "";
-//         clearInterval(clear);
-//         endGame();
-//     }
-// };
+    if (currentTime == 0){
+        alert("Time's Up!");
+        quizContainer.innerHTML = "";
+        clearInterval(clear);
+        setTimeout(endGame, 60000);
+    }
+};
 
-// function showCurrentQuestion(){
-//     var question=questionList[currentQuestion];
-//     quizContainer.innerHTML="";
+function showCurrentQuestion(){
+    var question=questionList[currentQuestion];
+    quizContainer.innerHTML="";
 
-//     var questionTitle = document.createElement("h1");
-//     questionTitle.innerText = question.question;
-//     quizContainer.appendChild(questionTitle);
+    var questionTitle = document.createElement("h2");
+    questionTitle.innerText = question.question;
+    quizContainer.appendChild(questionTitle);
 
-//     var choicesList = document.createElement("ul");
+    var choicesList = document.createElement("ul");
 
-//     for (var i=0; i=question.choices.length; i++){
-//         var questionLi = document.createElement("li");
-//         questionLi.innerText = question.choices[i];
-//         choicesList.appendChild(questionLi);
-//     }
+    for (var i=0; i<question.choices.length; i++){
+        var questionLi = document.createElement("li");
+        questionLi.innerText = question.choices[i];
+        choicesList.appendChild(questionLi);
+    }
 
-//     quizContainer.appendChild(choicesList);
-// }
+    quizContainer.appendChild(choicesList);
+}
 
-// quizContainer.addEventListener("click", function(event){
-//     if (event.target.matches("li")) {
-//         var answerClick = event.target.innerText;
+quizContainer.addEventListener("click", function(event){
+    console.log('answer selected');
+    if (event.target.matches("li")) {
+        var answerClick = event.target.innerText;
 
-//         var question = questionList[currentQuestion];
-//         if (answerClick===question.answer){
-//             currentScore++;
-//             currentTime++;
-//         }
-//         else{
-//             currentTime-=5;
-//             alert("Wrong!");
-//             currentQuestion++;
-//         }
-//         if (currentQuestion>=questionList.length){
-//             clearInterval(clear);
-//             quizContainer.innerHTML ="";
-//             endGame();
-//         }
-//         else{
-//             showCurrentQuestion();
-//         }
-//     }
-// });
+        var question = questionList[currentQuestion];
+        if (answerClick===question.answer){
+            currentScore++;
+            currentTime++;
+        }
+        else{
+            currentTime-=5;
+            alert("Wrong!");
+            currentQuestion++;
+        }
+        if (currentQuestion>=questionList.length){
+            clearInterval(timeLeft);
+            quizContainer.innerHTML ="";
+            endGame();
+        }
+        else{
+            showCurrentQuestion();
+        }
+    }
+});
 
 // function endGame({
-//     countdown.innerHTML-"";
+//     timeLeft.innerHTML="";
 //     var userName = prompt("Enter your initials");
 //     var currentHighScore = localStorage.getItem("score");
 
